cd35065017903ab24b79b5e94032b8c2af560900
Sneh Patel <snehp491@gmail.com>
2022-06-05 17:42:56 -0400

    create partial areas for nav and add bootstrap

--- server.js
+++ server.js
@@ -1,11 +1,21 @@
-const express = require('express');
+const express = require('express')
+const app = express()
 
-const app = express();
-const port = 3000;
-//Serves static files (we need it to import a css file)
-app.use(express.static('public'))
-//Sets a basic route
-app.get('/', (req, res) => res.send('Start'));
+// handlebars
+const exphbs = require('express-handlebars');
+const Handlebars = require('handlebars')
 
-//Makes the app listen to port 3000
-app.listen(port, () => console.log(`App listening to port ${port}`));
+app.engine('handlebars', exphbs.engine({ defaultLayout: 'main'}));
+app.set('view engine', 'handlebars');
+
+// Choose a port to listen on
+const port = process.env.PORT || 3000;
+
+app.get('/', (req, res) => {
+  res.render('home', { test: 'Hello World' });
+})
+
+// Tell the app what port to listen on
+app.listen(port, () => {
+  console.log(`App listening on port ${port}!`);
+})
